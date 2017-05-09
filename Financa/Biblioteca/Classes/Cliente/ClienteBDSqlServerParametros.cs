4bc59c1037c2d1aecb9d36cd50a08c92af33edfe
Gabriel Soares <biel_system@hotmail.com>
2017-05-08 21:07:58 -0300

    Implementação
    Classes e conexão ao Banco de Dados.

---
+++ Financa/Biblioteca/Classes/Cliente/ClienteBDSqlServerParametros.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Biblioteca.Classes.Cliente
+{
+    public class ClienteBDSqlServerParametros : ConexaoSqlServer
+    {
+       public void Insert(Cliente cliente)
+        {
+            try
+            {
+                this.abrirConexao();
+                string sql="";
+                SqlCommand cmd = new SqlCommand(sql, this.sqlConn);
+                //executando a instrucao 
+                cmd.ExecuteNonQuery();
+                //liberando a memoria 
+                cmd.Dispose();
+                this.fecharConexao();
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("Erro ao conecar e inserir " + ex.Message);
+            }
+        }
+    }
+}
---
+++ Financa/Biblioteca/Classes/Conta.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca.Classes
+{
+    class Conta
+    {
+        private int Cod_conta;
+        private Double Salario_conta;
+        private DateTime Data_salario;
+        private Cliente Cod_client;
+        
+
+        public int Cod_Conta
+        {
+            get { return Cod_conta; }
+            set { Cod_conta = value; }
+        }
+
+        public Double Salario_Conta
+        {
+            get { return Salario_conta; }
+            set { Salario_conta = value; }
+        }
+
+        public DateTime Data_Salario
+        {
+            get { return Data_salario; }
+            set { Data_salario = value; }
+        }
+
+        public Cliente Cod_Cliente
+        {
+            get { return Cod_client; }
+            set { Cod_client = value; }
+        }
+    }
+}
